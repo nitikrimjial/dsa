f2ed5fdade729b07cef44f6ea13c5d513fc44c6f
tk_dr <77196229+nitikrimjial@users.noreply.github.com>
2025-04-25 19:14:34 +0530

    Time: 25 ms (62.05%), Space: 17.9 MB (81.65%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,28 +1,20 @@
 class Solution {
 public:
-    int help(vector<int>& c, int a, int n, vector<vector<int>>& dp) {
-        if(a == 0){
-            return 0;
+    int coinChange(vector<int>& c, int a) {
+        vector<int> dp(a + 1, INT_MAX - 1);
+        dp[0] = 0;
+        for(int i = 0; i < dp.size(); i++){
+            for(int j = 0; j < c.size(); j++){
+                if(i >= c[j]){
+                    dp[i] = min(dp[i], 1 + dp[i - c[j]]);
+                }
+            }
         }
-        if (a < 0 || n < 0){
-            return INT_MAX - 1;
-        }
-        if(dp[n][a] != -1){
-            return dp[n][a];
-        }
-        int one = help(c, a, n - 1, dp);
-        int two = 1 + help(c, a - c[n], n, dp);
-        dp[n][a] = min(one, two);
-        return dp[n][a];
-    }
-    int coinChange(vector<int>& c, int a){
-        vector<vector<int>> dp(c.size() + 1, vector<int> (a + 1, -1));
-        int ans = help(c, a, c.size() - 1, dp);
-        if(ans == INT_MAX - 1){
+        if(dp[a] == INT_MAX - 1){
             return -1;
         }
         else{
-            return ans;
+            return dp[a];
         }
     }
 };
