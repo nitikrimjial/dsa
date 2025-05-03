eaa12f8467a8e51712f101fc7dc0dbf3acfff3e1
tk_dr <77196229+nitikrimjial@users.noreply.github.com>
2025-05-03 19:13:12 +0530

    Time: 0 ms (100%), Space: 9.1 MB (31.01%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    int help(int i, string& s, vector<int>& dp){
-        if(i == s.length()) return 1;
-        if(s[i] == '0') return 0;
-        if(dp[i] != -1) return dp[i];
-
-        int ways = help(i + 1, s, dp);
-        if(i + 1 < s.length() && stoi(s.substr(i, 2)) <= 26){
-            ways += help(i + 2, s, dp);
-        }
-        return dp[i] = ways;
-    }
     int numDecodings(string s) {
         int n = s.length();
-        vector<int> dp(n, - 1);
-        return help(0, s, dp);
+        vector<int> dp(n + 1, 0);
+        dp[n] = 1;
+        for(int i = n - 1; i >= 0; i--){
+            if(s[i] == '0') dp[i] = 0;
+            else{
+                dp[i] = dp[i + 1];
+                if(i + 1 < n && stoi(s.substr(i, 2)) <= 26){
+                    dp[i] += dp[i + 2];
+                }
+            }
+        }
+        return dp[0];
     }
 };
