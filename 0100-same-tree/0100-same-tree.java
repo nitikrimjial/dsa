bf7183b5957f466b78bb9250b2eac988c379eb7c
tk_dr <77196229+nitikrimjial@users.noreply.github.com>
2025-10-07 12:45:35 +0530

    Time: 0 ms (100%), Space: 41.6 MB (6.46%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -13,14 +13,37 @@
  *     }
  * }
  */
+//  recursive dfs
+//  class Solution {
+//     public boolean isSameTree(TreeNode p, TreeNode q) {
+//         if(p == null && q == null) return true;
+
+//         if(p == null || q == null) return false;
+
+//         if(p.val != q.val) return false;
+
+//         return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+//     }
+// }
+//  interative bfs
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null) return true;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(p);
+        queue.offer(q);
 
-        if(p == null || q == null) return false;
+        while(!queue.isEmpty()){
+            TreeNode t1 = queue.poll();
+            TreeNode t2 = queue.poll();
 
-        if(p.val != q.val) return false;
+            if(t1 == null && t2 == null) continue;
+            if(t1 == null || t2 == null || t1.val != t2.val) return false;
 
-        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+            queue.offer(t1.left);
+            queue.offer(t2.left);
+            queue.offer(t1.right);
+            queue.offer(t2.right);
+        }
+        return true;
     }
 }
