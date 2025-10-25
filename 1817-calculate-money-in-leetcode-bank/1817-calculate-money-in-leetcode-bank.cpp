6746523d000ee429d2485026c66a0834a8e3a387
tk_dr <77196229+nitikrimjial@users.noreply.github.com>
2025-10-25 08:38:48 +0530

    Time: 0 ms (100%), Space: 7.9 MB (13.49%) - LeetHub

--- 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
+++ 1817-calculate-money-in-leetcode-bank/1817-calculate-money-in-leetcode-bank.cpp
@@ -1,16 +1,28 @@
+// brute force 
+// class Solution {
+// public:
+//     int totalMoney(int n) {
+//         int totalMoney = 0;
+//         int weekStart = 1;
+//         while(n > 0){
+//             int days = min(7, n);
+//             for(int i = 0; i < days; i++){
+//                 totalMoney += weekStart + i;
+//             }
+//             weekStart++;
+//             n -= days;
+//         }
+//         return totalMoney;
+//     }
+// };
 class Solution {
 public:
     int totalMoney(int n) {
-        int totalMoney = 0;
-        int weekStart = 1;
-        while(n > 0){
-            int days = min(7, n);
-            for(int i = 0; i < days; i++){
-                totalMoney += weekStart + i;
-            }
-            weekStart++;
-            n -= days;
-        }
-        return totalMoney;
+        int fullWeeks = n/7;
+        int remainingDays = n % 7;
+
+        int sum_fullWeeks = 28 * fullWeeks + 7 * (fullWeeks * (fullWeeks - 1))/2;
+        int sum_remaining  = remainingDays * (fullWeeks + 1) + (remainingDays * (remainingDays - 1))/2;
+        return sum_fullWeeks + sum_remaining;
     }
 };
