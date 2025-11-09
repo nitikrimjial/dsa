502a64c4414fd1b16b7f77e4fb3b33b0008744fb
tk_dr <77196229+nitikrimjial@users.noreply.github.com>
2025-11-09 16:45:56 +0530

    Time: 0 ms (100%), Space: 42.3 MB (14.07%) - LeetHub

--- 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.java
+++ 2288-count-operations-to-obtain-zero/2288-count-operations-to-obtain-zero.java
@@ -3,12 +3,13 @@ class Solution {
         int count = 0;
         while(num1 != 0 && num2 != 0 ){
         if(num1 >= num2){
-             num1 = num1 - num2;
+            count += num1 / num2;
+             num1 = num1 % num2;
         }
         else {
-            num2 = num2 - num1;
+            count += num2 / num1;
+            num2 = num2 % num1;
         }
-        count++;
         }
         return count;
     }
